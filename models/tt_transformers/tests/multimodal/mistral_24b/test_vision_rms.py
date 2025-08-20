28b249b95e9254b6a253ea68c73f8e5c2a3baa24
nikileshx <nikilesh.kumar@multicorewareinc.com>
2025-08-20 12:18:08 +0000

    Migrate mistral 24b to tt_transformers

--- models/tt_transformers/tests/multimodal/mistral_24b/test_vision_rms.py
+++ models/tt_transformers/tests/multimodal/mistral_24b/test_vision_rms.py
@@ -1,19 +1,13 @@
-# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
-
-# SPDX-License-Identifier: Apache-2.0
-
-from loguru import logger
-
-import torch
-import pytest
 import os
 
+import pytest
+import torch
+from loguru import logger
+
 import ttnn
-from models.experimental.mistral_24b.tt.rmsnorm import RMSNorm
-
-from models.utility_functions import comp_allclose, comp_pcc, skip_for_grayskull
-
+from models.tt_transformers.tt.multimodal.mistral_24b.rmsnorm import RMSNorm
 from models.tt_transformers.tt.model_config import ModelArgs
+from models.utility_functions import comp_allclose, comp_pcc, skip_for_grayskull
 
 
 @torch.no_grad()
