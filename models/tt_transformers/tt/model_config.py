c67f5921451f46423dec512a7c444d75c6f60a73
Jelle Schuhmacher <jelle.schuhmacher@flex.ai>
2025-08-20 14:30:37 +0000

    Unmerge

--- models/tt_transformers/tt/model_config.py
+++ models/tt_transformers/tt/model_config.py
@@ -31,7 +31,6 @@ from models.tt_transformers.tt.load_checkpoints import (
     load_hf_state_dict,
     load_meta_state_dict,
     reverse_permute,
-    standardize_hf_keys,
     standardize_hf_keys_multimodal,
 )
 from models.utility_functions import is_blackhole, is_wormhole_b0, nearest_32
@@ -1426,6 +1425,7 @@ class ModelArgs:
             self.embed_scale = self.dim**0.5
 
     def _set_params_from_dict(self, config, is_hf=False):
+        logger.info(config)
         eos_token_id = config.get("eos_token_id", None)
         self.image_token_index = config.get("image_token_index", 262144)
 
@@ -1545,6 +1545,10 @@ class ModelArgs:
 
         self.state_dict_text_prefix = self._get_text_prefix()
         self.is_multimodal = "vision_config" in config or self.is_vision()
+
+        logger.info(f"CONFIG: {config}")
+        if self.is_multimodal:
+            self._set_vision_params(config["vision_config"])
 
         self._set_model_specific_params()
 
@@ -1667,18 +1671,6 @@ class ModelArgs:
         # self.vision_num_cross_attention_layers = vision_config.get("num_cross_attention_layers", -1)
 
     def _set_hf_params(self, checkpoint_dir):
-        def merge_text_config(base_config):
-            text_config = base_config.get("text_config", {})
-            # Merge non-nested keys into text_config
-            text_config.update({k: v for k, v in base_config.items() if k not in ["text_config", "vision_config"]})
-            return text_config
-
-        def merge_vision_config(base_config):
-            vision_config = base_config.get("vision_config", {})
-            # Merge non-nested keys into vision_config
-            vision_config.update({k: v for k, v in base_config.items() if k not in ["text_config", "vision_config"]})
-            return vision_config
-
         if self.from_hf_url:
             # Special case Qwen2.5-VL models until they are fully integrated into a HF release
             if "Qwen/Qwen2.5-VL" in self.model_name:
@@ -1690,31 +1682,17 @@ class ModelArgs:
                 logger.info(
                     f"Loading state param for dummy {self.model_name} from {self.LOCAL_HF_PARAMS[self.model_name]}"
                 )
-                self.hf_config = AutoConfig.from_pretrained(self.LOCAL_HF_PARAMS[self.model_name]).to_dict()
+                self.hf_config = AutoConfig.from_pretrained(self.LOCAL_HF_PARAMS[self.model_name])
             else:
-                self.hf_config = AutoConfig.from_pretrained(self.CKPT_DIR).to_dict()
+                self.hf_config = AutoConfig.from_pretrained(self.CKPT_DIR)
 
-            if "text_config" in self.hf_config or "vision_config" in self.hf_config:
-                if "gemma-3-4b" in self.base_model_name:
-                    self._set_params_from_dict(self.hf_config, is_hf=True)
-                    if "vision_config" in self.hf_config:
-                        merged_vision_config = merge_vision_config(self.hf_config)
-                        self._set_vision_params(merged_vision_config)
-                else:
-                    merged_text_config = merge_text_config(self.hf_config)
-                    self._set_params_from_dict(merged_text_config, is_hf=True)
-                    if "vision_config" in self.hf_config:
-                        merged_vision_config = merge_vision_config(self.hf_config)
-                        self._set_vision_params(merged_vision_config)
-            else:
-                self._set_params_from_dict(self.hf_config, is_hf=True)
-
+            config = self.hf_config.to_dict()
         else:
             config_file = os.path.join(checkpoint_dir, "config.json")
             assert os.path.exists(config_file), f"config.json file not found at {config_file}"
             with open(config_file, "r") as f:
                 config = json.load(f)
-            self._set_params_from_dict(config, is_hf=True)
+        self._set_params_from_dict(config, is_hf=True)
 
     def __repr__(self):
         return f"""ModelArgs(
@@ -1739,18 +1717,7 @@ class ModelArgs:
         return self.vision_chunk_size > 0
 
     def get_state_dict_prefix(self, module_name, layer_num, is_vision=False):
-        if "gemma-3-4b" in self.model_name:
-            if is_vision:
-                text_prefix = "model.vision_tower.vision_model.encoder."
-
-            else:
-                # text_prefix = "model.language_model."
-
-                text_prefix = ""
-
-        else:
-            text_prefix = self.state_dict_text_prefix
-
+        text_prefix = self.state_dict_text_prefix
         layer_prefix = f"layers.{layer_num}." if layer_num is not None else ""
 
         module_map = {
@@ -1834,8 +1801,7 @@ class ModelArgs:
             if self.is_multimodal:
                 state_dict = standardize_hf_keys_multimodal(state_dict)
             else:
-                state_dict = standardize_hf_keys(state_dict)
-            state_dict = convert_hf_to_meta(state_dict, self.head_dim)
+                state_dict = convert_hf_to_meta(state_dict, self.head_dim)
 
         keys_dict = list(state_dict.keys())[:]
         remv = [f"layers.{i}." for i in list(range(self.n_layers, self.full_model_n_layers))]
