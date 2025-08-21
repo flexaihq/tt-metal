caad9b39061d83201681b98e9f2f7e7fad21238b
nikileshx <nikilesh.kumar@multicorewareinc.com>
2025-08-21 10:11:01 +0000

    Add unit tests to adopt tt_ccl

--- models/tt_transformers/tests/multimodal/mistral_24b/test_vision_mlp.py
+++ models/tt_transformers/tests/multimodal/mistral_24b/test_vision_mlp.py
@@ -9,9 +9,9 @@ import torch
 from loguru import logger
 
 import ttnn
-
-from models.experimental.mistral_24b.tt.vision_mlp import MistralTTVisionMLP as MLP
 from models.tt_transformers.tt.model_config import ModelArgs
+
+from models.tt_transformers.tt.multimodal.mistral_24b.vision_mlp import MistralTTVisionMLP as MLP
 from models.utility_functions import comp_allclose, comp_pcc, skip_for_grayskull
 
 
--- models/tt_transformers/tests/multimodal/mistral_24b/test_vision_rms.py
+++ models/tt_transformers/tests/multimodal/mistral_24b/test_vision_rms.py
@@ -5,8 +5,8 @@ import torch
 from loguru import logger
 
 import ttnn
-from models.tt_transformers.tt.multimodal.mistral_24b.rmsnorm import RMSNorm
 from models.tt_transformers.tt.model_config import ModelArgs
+from models.tt_transformers.tt.multimodal.mistral_24b.rmsnorm import RMSNorm
 from models.utility_functions import comp_allclose, comp_pcc, skip_for_grayskull
 
 
